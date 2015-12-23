a812a148dc20c0fb1cbb414deb04eb7fdf46d222
Manji Kerai <admin@thecodecentre.co.uk>
2015-12-23 16:03:55 +0000

    updated 8;

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-var fromDate;
-var toDate;
+var auth2;
 var id_token;
 var profile;
 
-$(document).ready(function() {
-	var socket = io.connect();
-
-	if(gapi.auth2.GoogleAuth.isSignedIn.get() == true)
-	{
-		googleUser = gapi.auth2.GoogleAuth.currentUser.get();
-		profile = googleUser.getBasicProfile();
-		console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-		console.log("Name: " + profile.getName());
-		console.log("Image URL: " + profile.getImageUrl());
-		console.log("Email: " + profile.getEmail());
-
-		// The ID token you need to pass to your backend:
-		id_token = googleUser.getAuthResponse().id_token;
-		socket.emit('authenticate', {idtoken: id_token, email: profile.getEmail()});		
-	}
-	
 function onSignIn(googleUser) {
 // Useful data for your client-side scripts:
 	profile = googleUser.getBasicProfile();
@@ -30,8 +12,44 @@ function onSignIn(googleUser) {
 
 	// The ID token you need to pass to your backend:
 	id_token = googleUser.getAuthResponse().id_token;
-	socket.emit('authenticate', {idtoken: id_token, email: profile.getEmail()});
 };
+
+/**
+ * Initializes the Sign-In client.
+ */
+var initClient = function() {
+    gapi.load('auth2', function(){
+        /**
+         * Retrieve the singleton for the GoogleAuth library and set up the
+         * client.
+         */
+        auth2 = gapi.auth2.init({
+            client_id: '7640409675-vo4hcd4ggr59sbs2sskn662ohrk3skt8.apps.googleusercontent.com'
+        });
+
+        // Attach the click handler to the sign-in button
+        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
+    });
+};
+
+/**
+ * Handle successful sign-ins.
+ */
+var onSuccess = function(user) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+};
+
+$(document).ready(function() {
+	var socket = io.connect();
+
+	socket.emit('authenticate', {idtoken: id_token, email: profile.getEmail()});
 	  
 	socket.on('authRequest', function(data){
 		$("#error").text("Please authenticate yourself first");
