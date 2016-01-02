191a9f705f5595661e0fb7e82fd805c86150dff2
Manji Kerai <admin@thecodecentre.co.uk>
2016-01-02 12:17:08 +0000

    updated session;

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var express = require('express'),
 //var cookieParser = require('cookie-parser');
 //app.use(cookieParser());
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
