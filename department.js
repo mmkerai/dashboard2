df6f15ff94f7c190020b3db9e63b53b0d9fd4ddd
Manji Kerai <admin@thecodecentre.co.uk>
2016-01-31 14:00:43 +0000

    updated newwindow;

--- department.js
+++ department.js
@@ -1,9 +1,34 @@
 var socket = io.connect();
+var auth2;
+var Gid_token;
+var profile;
+
+function onSignIn(googleUser) {
+// Useful data for your client-side scripts:
+	profile = googleUser.getBasicProfile();
+//	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//	console.log("Name: " + profile.getName());
+//	console.log("Image URL: " + profile.getImageUrl());
+	console.log("Email: " + profile.getEmail());
+
+	// The ID token you need to pass to your backend:
+	Gid_token = googleUser.getAuthResponse().id_token;
+	socket.emit('authenticate', {token: Gid_token, email: profile.getEmail()});
+}
 
 $(document).ready(function() {
 
 var did = decodeURIComponent(window.location.search.match(/(\?|&)did\=([^&]*)/)[2]);
 console.log("did is "+did);
+
+  	$("#g-signout").hide();
+
+	socket.on('authResponse', function(data){
+		$("#g-signout").show();
+		$("#gname").text(profile.getName());
+		$("#gprofile-image").attr({src: profile.getImageUrl()});
+		$("#error").text("");
+	});
 
 	socket.on('errorResponse', function(data){
 		$("#error").text(data);
@@ -81,6 +106,20 @@ function NewWin(htmlfile, name)		// open a new window
 				'toolbar=yes,location=no,status=no,menubar=yes,scrollbars=yes,resizable=yes,width='+WIDTH+',height='+HEIGHT+',top='+top+',left='+left);
 	winpop.focus();
 	return winpop;
+}
+
+function signOut() {
+	auth2 = gapi.auth2.getAuthInstance();
+	if(auth2 === 'undefined')
+		console.log("auth2 is undefined");
+	
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+		$("#g-signout").hide();
+
+	if(Gid_token !== 'undefined')
+		socket.emit('un-authenticate', {token: Gid_token, email: profile.getEmail()});
+	});
 }
 
 function toHHMMSS(seconds) {
