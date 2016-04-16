3b2cede0984e4bddb79530770097a22c896625c1
Manji Kerai <admin@thecodecentre.co.uk>
2016-04-16 21:34:30 +0100

    updated actcolour;

--- department.js
+++ department.js
@@ -78,7 +78,7 @@ function createDeptRow(tableid, id, name) {
 	{
 		row.insertCell(i);
 	}
-	row.cells[0].outerHTML = "<th width="30">"+name+"</th>";
+	row.cells[0].outerHTML = "<th width='32'>"+name+"</th>";
 	return row;
 }
 
