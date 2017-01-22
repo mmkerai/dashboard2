0ccdc02a0aef1c90f13737d4a72fccef4c4e630c
Manji Kerai <admin@thecodecentre.co.uk>
2017-01-22 11:00:49 +0000

    updated shrinkage;

--- h3g_utils.js
+++ h3g_utils.js
@@ -2,7 +2,7 @@
 
 var ChatStatus = ["Logged Out","Away","Available"];
 var csvfile = null;
-var RTAVersion = "RTA Dashboard v1.23";
+var RTAVersion = "RTA Dashboard v1.24";
 
 function readCookie(name)
 {
